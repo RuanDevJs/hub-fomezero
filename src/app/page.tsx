c61718c65a098222eaa22cd9ab08662b8d034e9d
RuanDevJs <ruanvelpidio@hotmail.com>
2025-08-28 20:02:30 -0300

    feature: Finished home landing page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,12 @@
 "use client"
-
+import Logo from "@/assets/logo.svg"
+import Button from "@/components/Button"
 
 import Donation from '@/components/Home/Donation'
 import Main from '@/components/Home/Main'
 import Sobre from '@/components/Home/Sobre'
+import Image from "next/image"
+import { Phone } from "phosphor-react"
 
 export default function Home() {
   return (
@@ -11,6 +14,28 @@ export default function Home() {
       <Main />
       <Donation />
       <Sobre />
+      <footer className='mt-20 bg-black px-14 py-12'>
+        <div className="flex items-center justify-between">
+          <div>
+            <Image src={Logo} width={272} height={272} draggable={false} alt="" />
+          </div>
+          <nav>
+            <ul className='flex items-center justify-center gap-10'>
+              <li className='flex items-center gap-2'>
+                <Phone size={32} color='#fff' />
+                <a className='block w-full font-normal text-zinc-50 text-lg'>
+                  +55 0800 555 777
+                </a>
+              </li>
+              <li>
+                <a className='block w-full font-normal text-zinc-50 text-lg'>
+                  contato@hubfomezero.org
+                </a>
+              </li>
+            </ul>
+          </nav>
+        </div>
+      </footer>
     </div>
   )
 }
